c1535d0d386b586055eec38c7c88316744c95ef3
sebastianselander <sebastian.selander@gmail.com>
2024-03-21 16:06:13 +0100

    add splitByMode

--- app/Util.hs
+++ app/Util.hs
@@ -21,3 +21,13 @@ setRed = setColor Red
 
 setReset :: Env ()
 setReset = liftIO $ setSGR [Reset, SetDefaultColor Foreground]
+
+splitByMode :: [(a, Mode)] -> ([a], [a])
+splitByMode = go [] [] 
+  where
+    go reads writes [] = (reverse reads, reverse writes)
+    go reads writes ((a, mode) : rest) = case mode of
+        Read -> go (a : reads) writes rest
+        ReadWrite -> go (a : reads) writes rest
+        Write -> go reads (a : writes) rest
+        Append -> go reads (a : writes) rest
