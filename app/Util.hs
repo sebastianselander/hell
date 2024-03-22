b96fcf1d81b8782d5da401e289ba6b211a69d2cd
sebastianselander <sebastian.selander@gmail.com>
2024-03-22 14:58:23 +0100

    add utility functions

--- app/Util.hs
+++ app/Util.hs
@@ -31,3 +31,16 @@ splitByMode = go [] []
         ReadWrite -> go (a : reads) writes rest
         Write -> go reads (a : writes) rest
         Append -> go reads (a : writes) rest
+
+threeToTwo :: (a,b,c) -> ((a,b), c)
+threeToTwo (a,b,c) = ((a,b), c)
+
+esequence :: [Either err a] -> Either [err] [a]
+esequence xs = case go [] [] xs of
+    ([], rights) -> Right rights
+    (errs, _) -> Left errs
+  where
+    go lefts rights [] = (lefts, rights)
+    go lefts rights (x:xs) = case x of
+        Right a -> go lefts (a : rights) xs
+        Left err -> go (err : lefts) [] xs
